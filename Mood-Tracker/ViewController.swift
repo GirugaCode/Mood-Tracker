9950b63d4afd67717fd4031e4e973785c4005831
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-11-08 14:38:44 -0800

    Initial Commit

---
+++ Mood-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mood-Tracker
+//
+//  Created by Ryan Nguyen on 11/8/18.
+//  Copyright © 2018 Danh Phu Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
